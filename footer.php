c6c14d4b20bacea2ef573e6bbcbe3e6a96f39438
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2020-06-07 17:42:19 -0400

    Change fontawesome icon class

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 		<footer role="contentinfo" id="footer" class="bg-blue-dark text-blue-light">
 			<div id="social">
 				<a href="http://katelynmueller.tumblr.com/">
-					<span title="Tumblr" class="fab fa-tumblr-square fa-icon"></span>
+					<span title="Tumblr" class="fab fa-tumblr-square fontawesome-icon"></span>
 				</a>
 				<a href="http://www.linkedin.com/in/katelynmueller">
-					<span title="LinkedIn" class="fab fa-linkedin fa-icon"></span>
+					<span title="LinkedIn" class="fab fa-linkedin fontawesome-icon"></span>
 				</a>
 			</div>
 			<div class="attribution wrapper">website and original content &#169; katelyn mueller - all rights reserved</div>
